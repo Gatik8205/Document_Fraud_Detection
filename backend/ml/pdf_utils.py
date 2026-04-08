a169a5f5502de6ba85895f65ecf6d91feb6f91cb
Gatik Yadav <gatikyadav8205@gmail.com>
2026-04-08 21:49:12 +0530

    commit before presentation

---
+++ backend/ml/pdf_utils.py
@@ -0,0 +1,24 @@
+import fitz
+from PIL import Image
+import io
+
+def convert_pdf_to_image(pdf_path:str)-> Image.Image:
+    """Convert firt page of pdf to PIL image"""
+    doc=fitz.open(pdf_path)
+    image_paths=[]
+
+    for page_num in range(len(doc)):
+        page=doc[page_num]
+        mat=fitz.Matrix(2.0,2.0)
+        pix=page.get_pixmap(matrix=mat)
+
+        img_bytes=pix.tobytes("png")
+        image= Image.open(io.BytesIO(img_bytes)).convert("RGB")
+        
+        page_filename=f"{os.path.splitext(os.path.basename(pdf_path))[0]}_page{page_num+1}.png"
+        page_path=os.path.join(output_dir, page_filename)
+        image.save(page_path)
+        image_paths.append(page_path)
+
+    doc.close()
+    return image_path
